d146d1be068e1801da910bae89274ec25487ba6b
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-15 20:07:44 +0600

    create new item and input field created with disabled button

--- js/app.js
+++ js/app.js
@@ -8,3 +8,25 @@ const playersBgColor = document.getElementsByClassName('player');
 for (const playersBg of playersBgColor) {
     playersBg.style.backgroundColor = 'rgba(57, 130, 233, 0.2)'
 }
+
+// create new list item
+document.getElementById('list-btn').addEventListener('click', function () {
+    const listItem = document.getElementById('list-item');
+    const li = document.createElement('li');
+    li.innerText = 'New Item';
+    listItem.appendChild(li);
+});
+
+
+// creat input field and create button, when click the button the number will increase by one . when field value is 5 then the button will be disabled.
+
+document.getElementById('count-input').setAttribute('value', '0');
+let count = 0;
+function countNumbers() {
+    count = count + 1;
+    if (count == 5) {
+        document.getElementById('count-btn').setAttribute('disabled', true);
+    }
+    const countInput = document.getElementById('count-input');
+    countInput.value = count;
+}
