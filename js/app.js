bf0cda436d1a6d5d56afbb8cc833ca46caf900a4
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-15 13:59:04 +0600

    top players background color changed by using js dom

--- js/app.js
+++ js/app.js
@@ -3,3 +3,8 @@
 document.getElementById('top-players-title').style.color = '#ED6B4F';
 document.getElementById('top-blogs-title').style.color = '#ED6B4F';
 
+// add background color in players section div
+const playersBgColor = document.getElementsByClassName('player');
+for (const playersBg of playersBgColor) {
+    playersBg.style.backgroundColor = 'rgba(57, 130, 233, 0.2)'
+}
